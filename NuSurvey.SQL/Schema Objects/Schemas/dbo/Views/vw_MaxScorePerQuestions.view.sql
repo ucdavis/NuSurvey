03c97407cfb20022ffd46f0850ef22b6b6dc3d76
Jason Sylvestre <jsylvestre@ucdavis.edu>
2011-05-09 14:15:06 -0700

    Changes to database view

--- NuSurvey.SQL/Schema Objects/Schemas/dbo/Views/vw_MaxScorePerQuestions.view.sql
+++ NuSurvey.SQL/Schema Objects/Schemas/dbo/Views/vw_MaxScorePerQuestions.view.sql
@@ -3,5 +3,6 @@ AS
 SELECT     dbo.Questions.CategoryId, dbo.Questions.id, MAX(dbo.Responses.Score) AS MaxScore
 FROM         dbo.Questions INNER JOIN
                       dbo.Responses ON dbo.Questions.id = dbo.Responses.QuestionId
+WHERE     (dbo.Questions.IsActive = 1)
 GROUP BY dbo.Questions.id, dbo.Questions.CategoryId, dbo.Responses.IsActive
 HAVING      (dbo.Responses.IsActive = 1)
