6c6bba2d08bb9cac0923de2c25fe722325a02c95
Jason Sylvestre <jsylvestre@ucdavis.edu>
2011-05-24 11:33:53 -0700

    WIP Adding ReCaptcha for password reset.

---
+++ NuSurvey.Web/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace NuSurvey.Web.Helpers
+{
+    public static class HtmlHelperExtensions
+    {
+        public static string GenerateCaptcha(this HtmlHelper helper)
+        {
+
+            var captchaControl = new Recaptcha.RecaptchaControl
+            {
+                ID = "recaptcha",
+                Theme = "clean",
+                PublicKey = ConfigurationManager.AppSettings["RecaptchaPublicKey"],
+                PrivateKey = ConfigurationManager.AppSettings["RecaptchaPrivateKey"]
+            };
+
+            var htmlWriter = new HtmlTextWriter(new StringWriter());
+
+            captchaControl.RenderControl(htmlWriter);
+
+            return htmlWriter.InnerWriter.ToString();
+        }
+    }
+}
