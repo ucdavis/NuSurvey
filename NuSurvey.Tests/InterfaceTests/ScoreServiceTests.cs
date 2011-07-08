13f81508af31ba0e4b1834fa6185073a3995d537
Jason Sylvestre <jsylvestre@ucdavis.edu>
2011-07-08 10:34:23 -0700

    WIP ScoreService Tests

--- NuSurvey.Tests/InterfaceTests/ScoreServiceTests.cs
+++ NuSurvey.Tests/InterfaceTests/ScoreServiceTests.cs
@@ -125,7 +125,7 @@ namespace NuSurvey.Tests.InterfaceTests
             #endregion Assert
         }
         #endregion Non-Open Ended Tests
-        #region Open Ended Whole Number Tests
+        #region Open Ended Whole Number Tests Scored
 
         [TestMethod]
         public void TestWholeNumberRangeOfNumbers()
@@ -319,7 +319,206 @@ namespace NuSurvey.Tests.InterfaceTests
             #endregion Assert
         }
 
-        #endregion Open Ended Whole Number Tests
+        #endregion Open Ended Whole Number Tests Scored
+        #region Open Ended Whole Number Tests Not Scored
+
+        [TestMethod]
+        public void TestWholeNumberRangeOfNumbersNotScored()
+        {
+            #region Arrange
+            SetupQuestions();
+            var qAndA = new QuestionAnswerParameter { QuestionId = 4 };
+
+            var dict = new Dictionary<string, int>(); //answer, expected score
+            dict.Add("-12", 2);
+            dict.Add("-1", 2);
+            dict.Add("0", 2);
+            dict.Add("1", 2);
+            dict.Add("2", 2);
+            dict.Add("3", 2);
+            dict.Add("4", 3);
+            dict.Add("5", 3);
+            dict.Add("6", 3);
+            dict.Add("7", 4);
+            dict.Add("8", 4);
+            dict.Add("9", 4);
+            dict.Add("10", 5);
+            dict.Add("11", 5);
+            dict.Add("12", 5);
+            dict.Add("13", 7);
+            dict.Add("14", 7);
+            dict.Add("15", 7);
+            dict.Add("16", 7);
+
+            #endregion Arrange
+
+            #region Act
+            foreach (var i in dict)
+            {
+                qAndA.Answer = i.Key;
+                var result = ScoreService.ScoreQuestion(QuestionRepository.Queryable, qAndA);
+                Assert.IsNotNull(result, string.Format("Unexpected value for {0}", i.Key));
+                Assert.AreEqual(0, result.Score, string.Format("Unexpected score for {0}", i.Key));
+                Assert.IsFalse(result.Invalid);
+                Assert.AreEqual(string.Empty, result.Message);
+                Assert.IsTrue(result.ResponseId > 0);
+            }
+            #endregion Act
+        }
+
+
+        [TestMethod]
+        public void TestAnswerNotWholeNumber1NotScored()
+        {
+            #region Arrange
+            SetupQuestions();
+            var qAndA = new QuestionAnswerParameter { QuestionId = 4, Answer = null };
+            #endregion Arrange
+
+            #region Act
+            var result = ScoreService.ScoreQuestion(QuestionRepository.Queryable, qAndA);
+            #endregion Act
+
+            #region Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Invalid);
+            Assert.AreEqual("Answer is required", result.Message);
+            Assert.AreEqual(0, result.Score);
+            Assert.AreEqual(0, result.ResponseId);
+            #endregion Assert
+        }
+
+        [TestMethod]
+        public void TestAnswerNotWholeNumber2NotScored()
+        {
+            #region Arrange
+            SetupQuestions();
+            var qAndA = new QuestionAnswerParameter { QuestionId = 4, Answer = string.Empty };
+            #endregion Arrange
+
+            #region Act
+            var result = ScoreService.ScoreQuestion(QuestionRepository.Queryable, qAndA);
+            #endregion Act
+
+            #region Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Invalid);
+            Assert.AreEqual("Answer is required", result.Message);
+            Assert.AreEqual(0, result.Score);
+            Assert.AreEqual(0, result.ResponseId);
+            #endregion Assert
+        }
+
+        [TestMethod]
+        public void TestAnswerNotWholeNumber3NotScored()
+        {
+            #region Arrange
+            SetupQuestions();
+            var qAndA = new QuestionAnswerParameter { QuestionId = 4, Answer = " " };
+            #endregion Arrange
+
+            #region Act
+            var result = ScoreService.ScoreQuestion(QuestionRepository.Queryable, qAndA);
+            #endregion Act
+
+            #region Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Invalid);
+            Assert.AreEqual("Answer is required", result.Message);
+            Assert.AreEqual(0, result.Score);
+            Assert.AreEqual(0, result.ResponseId);
+            #endregion Assert
+        }
+
+        [TestMethod]
+        public void TestAnswerNotWholeNumber4NotScored()
+        {
+            #region Arrange
+            SetupQuestions();
+            var qAndA = new QuestionAnswerParameter { QuestionId = 4, Answer = "ten" };
+            #endregion Arrange
+
+            #region Act
+            var result = ScoreService.ScoreQuestion(QuestionRepository.Queryable, qAndA);
+            #endregion Act
+
+            #region Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Invalid);
+            Assert.AreEqual("Answer must be a whole number", result.Message);
+            Assert.AreEqual(0, result.Score);
+            Assert.AreEqual(0, result.ResponseId);
+            #endregion Assert
+        }
+
+        [TestMethod]
+        public void TestAnswerNotWholeNumber5NotScored()
+        {
+            #region Arrange
+            SetupQuestions();
+            var qAndA = new QuestionAnswerParameter { QuestionId = 4, Answer = "3.5" };
+            #endregion Arrange
+
+            #region Act
+            var result = ScoreService.ScoreQuestion(QuestionRepository.Queryable, qAndA);
+            #endregion Act
+
+            #region Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Invalid);
+            Assert.AreEqual("Answer must be a whole number", result.Message);
+            Assert.AreEqual(0, result.Score);
+            Assert.AreEqual(0, result.ResponseId);
+            #endregion Assert
+        }
+
+        [TestMethod]
+        public void TestAnswerNotWholeNumber6NotScored()
+        {
+            #region Arrange
+            SetupQuestions();
+            var qAndA = new QuestionAnswerParameter { QuestionId = 4, Answer = "3:5" };
+            #endregion Arrange
+
+            #region Act
+            var result = ScoreService.ScoreQuestion(QuestionRepository.Queryable, qAndA);
+            #endregion Act
+
+            #region Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Invalid);
+            Assert.AreEqual("Answer must be a whole number", result.Message);
+            Assert.AreEqual(0, result.Score);
+            Assert.AreEqual(0, result.ResponseId);
+            #endregion Assert
+        }
+
+        [TestMethod]
+        public void TestAnswerNotWholeNumber7NotScored()
+        {
+            #region Arrange
+            SetupQuestions();
+            var qAndA = new QuestionAnswerParameter { QuestionId = 4, Answer = "3 5" };
+            #endregion Arrange
+
+            #region Act
+            var result = ScoreService.ScoreQuestion(QuestionRepository.Queryable, qAndA);
+            #endregion Act
+
+            #region Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Invalid);
+            Assert.AreEqual("Answer must be a whole number", result.Message);
+            Assert.AreEqual(0, result.Score);
+            Assert.AreEqual(0, result.ResponseId);
+            #endregion Assert
+        }
+
+        #endregion Open Ended Whole Number Tests Not Scored
+        #region Open Ended Decimal Tests
+        
+
+        #endregion Open Ended Decimal Tests
 
         #endregion Score Question Tests
 
@@ -344,7 +543,7 @@ namespace NuSurvey.Tests.InterfaceTests
         private void SetupQuestions()
         {
             var questions = new List<Question>();
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 10; i++)
             {
                 questions.Add(CreateValidEntities.Question(i + 1));
             }
@@ -383,6 +582,17 @@ namespace NuSurvey.Tests.InterfaceTests
             questions[2].IsOpenEnded = true;
             questions[2].OpenEndedQuestionType = (int)QuestionType.WholeNumber;
             #endregion Question 3 (Open Ended Whole Number)
+
+            #region Question 4 (Open Ended Whole Number No Scoreing)
+            questions[3].Name = "Whole Number";
+            foreach (var response in WholeNumberResponses())
+            {
+                questions[3].AddResponse(response);
+            }
+            questions[3].IsOpenEnded = true;
+            questions[3].OpenEndedQuestionType = (int)QuestionType.WholeNumber;
+            questions[3].Category.DoNotUseForCalculations = true;
+            #endregion Question 4 (Open Ended Whole Number No Scoreing)
 
             new FakeQuestions(0, QuestionRepository, questions);
         }       
