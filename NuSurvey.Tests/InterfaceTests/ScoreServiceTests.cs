98665401aa67f55c69425e7bdd8152a232636669
Jason Sylvestre <jsylvestre@ucdavis.edu>
2011-07-08 10:26:17 -0700

    WIP ScoreService Tests

--- NuSurvey.Tests/InterfaceTests/ScoreServiceTests.cs
+++ NuSurvey.Tests/InterfaceTests/ScoreServiceTests.cs
@@ -125,6 +125,201 @@ namespace NuSurvey.Tests.InterfaceTests
             #endregion Assert
         }
         #endregion Non-Open Ended Tests
+        #region Open Ended Whole Number Tests
+
+        [TestMethod]
+        public void TestWholeNumberRangeOfNumbers()
+        {
+            #region Arrange
+            SetupQuestions();
+            var qAndA = new QuestionAnswerParameter {QuestionId = 3};
+
+            var dict = new Dictionary<string, int>(); //answer, expected score
+            dict.Add("-12", 2);
+            dict.Add("-1", 2);
+            dict.Add("0", 2);
+            dict.Add("1", 2);
+            dict.Add("2", 2);
+            dict.Add("3", 2);
+            dict.Add("4", 3);
+            dict.Add("5", 3); 
+            dict.Add("6", 3);
+            dict.Add("7", 4);
+            dict.Add("8", 4);
+            dict.Add("9", 4);
+            dict.Add("10", 5);
+            dict.Add("11", 5);
+            dict.Add("12", 5);
+            dict.Add("13", 7);
+            dict.Add("14", 7);
+            dict.Add("15", 7);
+            dict.Add("16", 7);
+
+            #endregion Arrange
+
+            #region Act
+            foreach (var i in dict)
+            {
+                qAndA.Answer = i.Key;
+                var result = ScoreService.ScoreQuestion(QuestionRepository.Queryable, qAndA);
+                Assert.IsNotNull(result, string.Format("Unexpected value for {0}", i.Key));
+                Assert.AreEqual(i.Value, result.Score, string.Format("Unexpected score for {0}", i.Key));
+                Assert.IsFalse(result.Invalid);
+                Assert.AreEqual(string.Empty, result.Message);
+                Assert.IsTrue(result.ResponseId > 0);
+            }
+            #endregion Act		
+        }
+
+
+        [TestMethod]
+        public void TestAnswerNotWholeNumber1()
+        {
+            #region Arrange
+            SetupQuestions();
+            var qAndA = new QuestionAnswerParameter {QuestionId = 3, Answer = null};
+            #endregion Arrange
+
+            #region Act
+            var result = ScoreService.ScoreQuestion(QuestionRepository.Queryable, qAndA);
+            #endregion Act
+
+            #region Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Invalid);
+            Assert.AreEqual("Answer is required", result.Message);
+            Assert.AreEqual(0, result.Score);
+            Assert.AreEqual(0, result.ResponseId);
+            #endregion Assert		
+        }
+
+        [TestMethod]
+        public void TestAnswerNotWholeNumber2()
+        {
+            #region Arrange
+            SetupQuestions();
+            var qAndA = new QuestionAnswerParameter { QuestionId = 3, Answer = string.Empty };
+            #endregion Arrange
+
+            #region Act
+            var result = ScoreService.ScoreQuestion(QuestionRepository.Queryable, qAndA);
+            #endregion Act
+
+            #region Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Invalid);
+            Assert.AreEqual("Answer is required", result.Message);
+            Assert.AreEqual(0, result.Score);
+            Assert.AreEqual(0, result.ResponseId);
+            #endregion Assert
+        }
+
+        [TestMethod]
+        public void TestAnswerNotWholeNumber3()
+        {
+            #region Arrange
+            SetupQuestions();
+            var qAndA = new QuestionAnswerParameter { QuestionId = 3, Answer = " " };
+            #endregion Arrange
+
+            #region Act
+            var result = ScoreService.ScoreQuestion(QuestionRepository.Queryable, qAndA);
+            #endregion Act
+
+            #region Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Invalid);
+            Assert.AreEqual("Answer is required", result.Message);
+            Assert.AreEqual(0, result.Score);
+            Assert.AreEqual(0, result.ResponseId);
+            #endregion Assert
+        }
+
+        [TestMethod]
+        public void TestAnswerNotWholeNumber4()
+        {
+            #region Arrange
+            SetupQuestions();
+            var qAndA = new QuestionAnswerParameter { QuestionId = 3, Answer = "ten" };
+            #endregion Arrange
+
+            #region Act
+            var result = ScoreService.ScoreQuestion(QuestionRepository.Queryable, qAndA);
+            #endregion Act
+
+            #region Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Invalid);
+            Assert.AreEqual("Answer must be a whole number", result.Message);
+            Assert.AreEqual(0, result.Score);
+            Assert.AreEqual(0, result.ResponseId);
+            #endregion Assert
+        }
+
+        [TestMethod]
+        public void TestAnswerNotWholeNumber5()
+        {
+            #region Arrange
+            SetupQuestions();
+            var qAndA = new QuestionAnswerParameter { QuestionId = 3, Answer = "3.5" };
+            #endregion Arrange
+
+            #region Act
+            var result = ScoreService.ScoreQuestion(QuestionRepository.Queryable, qAndA);
+            #endregion Act
+
+            #region Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Invalid);
+            Assert.AreEqual("Answer must be a whole number", result.Message);
+            Assert.AreEqual(0, result.Score);
+            Assert.AreEqual(0, result.ResponseId);
+            #endregion Assert
+        }
+
+        [TestMethod]
+        public void TestAnswerNotWholeNumber6()
+        {
+            #region Arrange
+            SetupQuestions();
+            var qAndA = new QuestionAnswerParameter { QuestionId = 3, Answer = "3:5" };
+            #endregion Arrange
+
+            #region Act
+            var result = ScoreService.ScoreQuestion(QuestionRepository.Queryable, qAndA);
+            #endregion Act
+
+            #region Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Invalid);
+            Assert.AreEqual("Answer must be a whole number", result.Message);
+            Assert.AreEqual(0, result.Score);
+            Assert.AreEqual(0, result.ResponseId);
+            #endregion Assert
+        }
+
+        [TestMethod]
+        public void TestAnswerNotWholeNumber7()
+        {
+            #region Arrange
+            SetupQuestions();
+            var qAndA = new QuestionAnswerParameter { QuestionId = 3, Answer = "3 5" };
+            #endregion Arrange
+
+            #region Act
+            var result = ScoreService.ScoreQuestion(QuestionRepository.Queryable, qAndA);
+            #endregion Act
+
+            #region Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Invalid);
+            Assert.AreEqual("Answer must be a whole number", result.Message);
+            Assert.AreEqual(0, result.Score);
+            Assert.AreEqual(0, result.ResponseId);
+            #endregion Assert
+        }
+
+        #endregion Open Ended Whole Number Tests
 
         #endregion Score Question Tests
 
@@ -193,7 +388,7 @@ namespace NuSurvey.Tests.InterfaceTests
         }       
 
 
-        private static IList<Response> WholeNumberResponses()
+        private static IEnumerable<Response> WholeNumberResponses()
         {
             var responses = new List<Response>();
             for (int i = 0; i < 7; i++)
