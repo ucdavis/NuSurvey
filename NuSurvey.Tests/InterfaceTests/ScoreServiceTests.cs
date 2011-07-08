e052558a52b0d3a71d98a752b4731522db40092a
Jason Sylvestre <jsylvestre@ucdavis.edu>
2011-07-08 11:21:07 -0700

    WIP ScoreService Tests

--- NuSurvey.Tests/InterfaceTests/ScoreServiceTests.cs
+++ NuSurvey.Tests/InterfaceTests/ScoreServiceTests.cs
@@ -20,6 +20,7 @@ namespace NuSurvey.Tests.InterfaceTests
         {
             ScoreService = new ScoreService();
             QuestionRepository = MockRepository.GenerateStub<IRepository<Question>>();
+            SetupQuestions();
         }
 
         #region Score Question Tests
@@ -29,7 +30,7 @@ namespace NuSurvey.Tests.InterfaceTests
         public void TestWhenQuestionHasNoAnswer()
         {
             #region Arrange
-            SetupQuestions();
+            //SetupQuestions();
             var qAndA = new QuestionAnswerParameter {QuestionId = 1, ResponseId = 0};
             #endregion Arrange
 
@@ -49,7 +50,7 @@ namespace NuSurvey.Tests.InterfaceTests
         public void TestWhenQuestionHasWrongAnswer()
         {
             #region Arrange
-            SetupQuestions();
+            //SetupQuestions();
             var qAndA = new QuestionAnswerParameter {QuestionId = 1, ResponseId = 2};
             #endregion Arrange
 
@@ -69,7 +70,7 @@ namespace NuSurvey.Tests.InterfaceTests
         public void TestWhenQuestionHasCorrectAnswer1()
         {
             #region Arrange
-            SetupQuestions();
+            //SetupQuestions();
             var qAndA = new QuestionAnswerParameter { QuestionId = 1, ResponseId = 3 };
             #endregion Arrange
 
@@ -89,7 +90,7 @@ namespace NuSurvey.Tests.InterfaceTests
         public void TestWhenQuestionHasCorrectAnswer2()
         {
             #region Arrange
-            SetupQuestions();
+            //SetupQuestions();
             var qAndA = new QuestionAnswerParameter { QuestionId = 1, ResponseId = 4 };
             #endregion Arrange
 
@@ -109,7 +110,7 @@ namespace NuSurvey.Tests.InterfaceTests
         public void TestWhenQuestionHasCorrectAnswer3()
         {
             #region Arrange
-            SetupQuestions();
+            //SetupQuestions();
             var qAndA = new QuestionAnswerParameter { QuestionId = 2, ResponseId = 4 };
             #endregion Arrange
 
@@ -131,7 +132,7 @@ namespace NuSurvey.Tests.InterfaceTests
         public void TestWholeNumberRangeOfNumbers()
         {
             #region Arrange
-            SetupQuestions();
+            //SetupQuestions();
             var qAndA = new QuestionAnswerParameter {QuestionId = 3};
 
             var dict = new Dictionary<string, int>(); //answer, expected score
@@ -176,7 +177,7 @@ namespace NuSurvey.Tests.InterfaceTests
         public void TestAnswerNotWholeNumber1()
         {
             #region Arrange
-            SetupQuestions();
+            //SetupQuestions();
             var qAndA = new QuestionAnswerParameter {QuestionId = 3, Answer = null};
             #endregion Arrange
 
@@ -197,7 +198,7 @@ namespace NuSurvey.Tests.InterfaceTests
         public void TestAnswerNotWholeNumber2()
         {
             #region Arrange
-            SetupQuestions();
+            //SetupQuestions();
             var qAndA = new QuestionAnswerParameter { QuestionId = 3, Answer = string.Empty };
             #endregion Arrange
 
@@ -218,7 +219,7 @@ namespace NuSurvey.Tests.InterfaceTests
         public void TestAnswerNotWholeNumber3()
         {
             #region Arrange
-            SetupQuestions();
+            //SetupQuestions();
             var qAndA = new QuestionAnswerParameter { QuestionId = 3, Answer = " " };
             #endregion Arrange
 
@@ -239,7 +240,7 @@ namespace NuSurvey.Tests.InterfaceTests
         public void TestAnswerNotWholeNumber4()
         {
             #region Arrange
-            SetupQuestions();
+            //SetupQuestions();
             var qAndA = new QuestionAnswerParameter { QuestionId = 3, Answer = "ten" };
             #endregion Arrange
 
@@ -260,7 +261,7 @@ namespace NuSurvey.Tests.InterfaceTests
         public void TestAnswerNotWholeNumber5()
         {
             #region Arrange
-            SetupQuestions();
+            //SetupQuestions();
             var qAndA = new QuestionAnswerParameter { QuestionId = 3, Answer = "3.5" };
             #endregion Arrange
 
@@ -281,7 +282,7 @@ namespace NuSurvey.Tests.InterfaceTests
         public void TestAnswerNotWholeNumber6()
         {
             #region Arrange
-            SetupQuestions();
+            //SetupQuestions();
             var qAndA = new QuestionAnswerParameter { QuestionId = 3, Answer = "3:5" };
             #endregion Arrange
 
@@ -302,7 +303,7 @@ namespace NuSurvey.Tests.InterfaceTests
         public void TestAnswerNotWholeNumber7()
         {
             #region Arrange
-            SetupQuestions();
+            //SetupQuestions();
             var qAndA = new QuestionAnswerParameter { QuestionId = 3, Answer = "3 5" };
             #endregion Arrange
 
@@ -326,7 +327,7 @@ namespace NuSurvey.Tests.InterfaceTests
         public void TestWholeNumberRangeOfNumbersNotScored()
         {
             #region Arrange
-            SetupQuestions();
+            //SetupQuestions();
             var qAndA = new QuestionAnswerParameter { QuestionId = 4 };
 
             var dict = new Dictionary<string, int>(); //answer, expected score
@@ -371,7 +372,7 @@ namespace NuSurvey.Tests.InterfaceTests
         public void TestAnswerNotWholeNumber1NotScored()
         {
             #region Arrange
-            SetupQuestions();
+            //SetupQuestions();
             var qAndA = new QuestionAnswerParameter { QuestionId = 4, Answer = null };
             #endregion Arrange
 
@@ -392,7 +393,7 @@ namespace NuSurvey.Tests.InterfaceTests
         public void TestAnswerNotWholeNumber2NotScored()
         {
             #region Arrange
-            SetupQuestions();
+            //SetupQuestions();
             var qAndA = new QuestionAnswerParameter { QuestionId = 4, Answer = string.Empty };
             #endregion Arrange
 
@@ -413,7 +414,7 @@ namespace NuSurvey.Tests.InterfaceTests
         public void TestAnswerNotWholeNumber3NotScored()
         {
             #region Arrange
-            SetupQuestions();
+            //SetupQuestions();
             var qAndA = new QuestionAnswerParameter { QuestionId = 4, Answer = " " };
             #endregion Arrange
 
@@ -434,7 +435,7 @@ namespace NuSurvey.Tests.InterfaceTests
         public void TestAnswerNotWholeNumber4NotScored()
         {
             #region Arrange
-            SetupQuestions();
+            //SetupQuestions();
             var qAndA = new QuestionAnswerParameter { QuestionId = 4, Answer = "ten" };
             #endregion Arrange
 
@@ -455,7 +456,7 @@ namespace NuSurvey.Tests.InterfaceTests
         public void TestAnswerNotWholeNumber5NotScored()
         {
             #region Arrange
-            SetupQuestions();
+            //SetupQuestions();
             var qAndA = new QuestionAnswerParameter { QuestionId = 4, Answer = "3.5" };
             #endregion Arrange
 
@@ -476,7 +477,7 @@ namespace NuSurvey.Tests.InterfaceTests
         public void TestAnswerNotWholeNumber6NotScored()
         {
             #region Arrange
-            SetupQuestions();
+            //SetupQuestions();
             var qAndA = new QuestionAnswerParameter { QuestionId = 4, Answer = "3:5" };
             #endregion Arrange
 
@@ -497,7 +498,7 @@ namespace NuSurvey.Tests.InterfaceTests
         public void TestAnswerNotWholeNumber7NotScored()
         {
             #region Arrange
-            SetupQuestions();
+            //SetupQuestions();
             var qAndA = new QuestionAnswerParameter { QuestionId = 4, Answer = "3 5" };
             #endregion Arrange
 
@@ -516,7 +517,62 @@ namespace NuSurvey.Tests.InterfaceTests
 
         #endregion Open Ended Whole Number Tests Not Scored
         #region Open Ended Decimal Tests
-        
+        [TestMethod]
+        public void TestDecimalNumberRangeOfNumbers()
+        {
+            #region Arrange
+            //SetupQuestions();
+            var qAndA = new QuestionAnswerParameter { QuestionId = 5 };
+
+            var dict = new Dictionary<string, int>(); //answer, expected score
+            dict.Add("-12", 1);
+            dict.Add("-1.3", 1);
+            dict.Add("-1.2", 1);
+            dict.Add("-1.1", 1);
+            dict.Add("-0.2", 2);
+            dict.Add("0", 2);
+            dict.Add("0.74", 2);
+            dict.Add("0.7499", 2);
+            dict.Add("0.75", 3);
+            dict.Add("1", 3);
+            dict.Add("1.5", 3);
+            dict.Add("1.7499", 3);
+            dict.Add("1.75", 4);
+            dict.Add("1.999", 4);
+            dict.Add("2", 4);
+            dict.Add("2.4999", 4);
+            dict.Add("2.5", 5);
+            dict.Add("2.999", 5);
+            dict.Add("3", 5);
+            dict.Add("3.624999", 5);
+            dict.Add("3.625", 6);
+            dict.Add("4.25", 6);
+            dict.Add("4.37499", 6);
+            dict.Add("4.375", 7);
+            dict.Add("4.5", 7);
+            dict.Add("5", 7);
+            dict.Add("5.24999", 7);
+            dict.Add("5.25", 9);
+            dict.Add("6.00", 9);
+            dict.Add("6.00499", 9);
+            dict.Add("6.005", 10);
+            dict.Add("6.01", 10);
+            dict.Add("123.123", 10);
+            #endregion Arrange
+
+            #region Act
+            foreach (var i in dict)
+            {
+                qAndA.Answer = i.Key;
+                var result = ScoreService.ScoreQuestion(QuestionRepository.Queryable, qAndA);
+                Assert.IsNotNull(result, string.Format("Unexpected value for {0}", i.Key));
+                Assert.AreEqual(i.Value, result.Score, string.Format("Unexpected score for {0}", i.Key));
+                Assert.IsFalse(result.Invalid, string.Format("Unexpected score for {0}", i.Key));
+                Assert.AreEqual(string.Empty, result.Message);
+                Assert.IsTrue(result.ResponseId > 0);
+            }
+            #endregion Act
+        }
 
         #endregion Open Ended Decimal Tests
 
@@ -594,6 +650,16 @@ namespace NuSurvey.Tests.InterfaceTests
             questions[3].Category.DoNotUseForCalculations = true;
             #endregion Question 4 (Open Ended Whole Number No Scoreing)
 
+            #region Question 5 (Open Ended Decimal)
+            questions[4].Name = "Whole Number";
+            foreach (var response in DecimalResponses())
+            {
+                questions[4].AddResponse(response);
+            }
+            questions[4].IsOpenEnded = true;
+            questions[4].OpenEndedQuestionType = (int)QuestionType.Decimal;
+            #endregion Question 5 (Open Ended Decimal)
+
             new FakeQuestions(0, QuestionRepository, questions);
         }       
 
@@ -627,6 +693,46 @@ namespace NuSurvey.Tests.InterfaceTests
             scrambledResponses.Add(responses[3]);
             scrambledResponses.Add(responses[6]);
 
+            return scrambledResponses;
+        }
+
+        private static IEnumerable<Response> DecimalResponses()
+        {
+            var responses = new List<Response>();
+            for (int i = 0; i < 11; i++)
+            {
+                responses.Add(CreateValidEntities.Response(i + 1));
+                responses[i].Score = i + 1;
+                responses[i].SetIdTo(i + 1);
+            }
+
+            responses[0].Value = "-1.2";
+            responses[1].Value = "0";
+            responses[2].Value = "1.5";
+            responses[3].Value = "2";
+            responses[4].Value = "3";
+            responses[5].Value = "4.25";
+            responses[6].Value = "4.5";
+            responses[7].Value = "5";
+            responses[7].IsActive = false;
+            responses[8].Value = "6";
+            responses[9].Value = "6.01";
+
+            responses[10].Value = "TEN";
+            responses[10].Score = 99;
+
+            var scrambledResponses = new List<Response>(); //Because the service sorts.
+            scrambledResponses.Add(responses[6]);
+            scrambledResponses.Add(responses[5]);
+            scrambledResponses.Add(responses[3]);
+            scrambledResponses.Add(responses[1]);
+            scrambledResponses.Add(responses[2]);
+            scrambledResponses.Add(responses[7]);
+            scrambledResponses.Add(responses[0]);
+            scrambledResponses.Add(responses[8]);
+            scrambledResponses.Add(responses[4]);
+            scrambledResponses.Add(responses[10]);
+            scrambledResponses.Add(responses[9]);
             return scrambledResponses;
         }
         #endregion Helper Methods
