36a71c6d27e2be429b75e4aa6b2f401c752ec6b7
Jason Sylvestre <jsylvestre@ucdavis.edu>
2014-07-08 10:13:48 -0700

    Have it go to a page to set the name, then redirect to the select photos page.

--- NuSurvey.Web/Controllers/ProgramDirectorController.cs
+++ NuSurvey.Web/Controllers/ProgramDirectorController.cs
@@ -81,9 +81,9 @@ namespace NuSurvey.Web.Controllers
 
             _printedSurveyRepository.EnsurePersistent(printedSurvey);
 
-            return this.RedirectToAction(a => a.SelectPhotos(printedSurvey.Id));
+            return this.RedirectToAction(a => a.SetName(printedSurvey.Id));
 
-            //return this.RedirectToAction(a => a.Index());
+
         }
 
         [ProgramDirector]
@@ -95,12 +95,18 @@ namespace NuSurvey.Web.Controllers
             return View(printedSurvey);
         }
 
-        //[ProgramDirector]
-        //[HttpPost]
-        //public ActionResult SetName(int id, PrintedSurvey printedSurvey)
-        //{
-        //    throw new NotImplementedException("Do this");
-        //}
+        [ProgramDirector]
+        [HttpPost]
+        public ActionResult SetName(int id, PrintedSurvey printedSurvey)
+        {
+            var userId = CurrentUser.Identity.Name;
+            var printedSurveyToEdit = _printedSurveyRepository.Queryable.Single(a => a.Id == id && a.UserId == userId);
+
+            printedSurveyToEdit.Name = printedSurvey.Name;
+            _printedSurveyRepository.EnsurePersistent(printedSurveyToEdit);
+
+            return this.RedirectToAction(a => a.SelectPhotos(id));
+        }
 
         [ProgramDirector]
         public ActionResult Delete(int id)
