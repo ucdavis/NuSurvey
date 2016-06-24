effc299cbd9276875eb165b565bff334257c846a
Jason Sylvestre <jsylvestre@ucdavis.edu>
2016-06-24 09:14:57 -0700

    SpanishHelper and specific text on the results page.

---
+++ NuSurvey.MVC/Helpers/SpanishHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NuSurvey.MVC.Helpers
+{
+    public static class SpanishHelper
+    {
+        public static bool IsSpanish(this string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return false;
+            }
+            if (value.Trim().EndsWith("S", StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
